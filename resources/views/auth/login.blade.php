27ed1144024bd04d7fb775e6777aa102d8b6d55a
alexmuelas <alex.muelas.92@hotmail.com>
2019-09-26 18:28:10 +0000

    Navbar/Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -77,5 +77,131 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+  <div class="wrapper wrapper-full-page">
+    <div class="page-header header-filter" filter-color="black" style="background-image: url({{ asset('img/login.jpg') }}); background-size: cover; background-position: top center;">
+      <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
+          <form method="POST" action="{{ route('login') }}">              
+          @csrf  
+            <div class="card card-login card-hidden">
+                <div class="card-header card-header-rose text-center">
+                  <h4 class="card-title">Login</h4>
+                  <div class="social-line">
+                    <a href="#pablo" class="btn btn-just-icon btn-link btn-white">
+                      <i class="fa fa-facebook-square"></i>
+                    </a>
+                    <a href="#pablo" class="btn btn-just-icon btn-link btn-white">
+                      <i class="fa fa-twitter"></i>
+                    </a>
+                    <a href="#pablo" class="btn btn-just-icon btn-link btn-white">
+                      <i class="fa fa-google-plus"></i>
+                    </a>
+                  </div>
+                </div>
+                <div class="card-body ">
+                  <!-- <p class="card-description text-center">Or Be Classical</p>
+                  <span class="bmd-form-group">
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">
+                          <i class="material-icons">face</i>
+                        </span>
+                      </div>
+                      <input type="text" class="form-control" placeholder="First Name...">
+                    </div>
+                  </span> -->
+                  <span class="bmd-form-group">
+
+                  <!-- <div class="form-group row">
+                            <label for="login" class="col-sm-4 col-form-label text-md-right">
+                                {{ __('Username or Email') }}
+                            </label>
+
+                            <div class="col-md-6">
+                                <input id="login" type="text"
+                                    class="form-control{{ $errors->has('user_name') || $errors->has('email') ? ' is-invalid' : '' }}"
+                                    name="login" value="{{ old('user_name') ?: old('email') }}" required autofocus>
+
+                                @if ($errors->has('user_name') || $errors->has('email'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('user_name') ?: $errors->first('email') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                        </div> -->
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">
+                          <i class="material-icons">email</i>
+                        </span>
+                      </div>
+                      <input id="login" type="text"
+                                    class="form-control{{ $errors->has('user_name') || $errors->has('email') ? ' is-invalid' : '' }}"
+                                    name="login" value="{{ old('user_name') ?: old('email') }}" required autofocus>
+                    </div>
+                  </span>
+                  <span class="bmd-form-group">
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">
+                          <i class="material-icons">lock_outline</i>
+                        </span>
+                      </div>
+                      <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    required autocomplete="current-password">                    </div>
+                  </span>
+                </div>
+                <div class="card-footer justify-content-center">
+                  <!-- <a href="#pablo" class="btn btn-rose btn-link btn-lg">Lets Go</a> -->
+                  <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <footer class="footer">
+        <div class="container">
+          <nav class="float-left">
+            <ul>
+              <li>
+                <a href="https://www.creative-tim.com">
+                  Creative Tim
+                </a>
+              </li>
+              <li>
+                <a href="https://creative-tim.com/presentation">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a href="http://blog.creative-tim.com">
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a href="https://www.creative-tim.com/license">
+                  Licenses
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div class="copyright float-right">
+            &copy;
+            <script>
+              document.write(new Date().getFullYear())
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> for a better web.
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
 @endsection
